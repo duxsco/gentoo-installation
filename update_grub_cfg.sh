f9de91db936aa5dd4cef3a332a874029ee18baa5
David Sardari <d@duxsco.de>
2022-01-17 23:33:59 +0100

    Mount /boot for grub.cfg update

--- update_grub_cfg.sh
+++ update_grub_cfg.sh
@@ -5,22 +5,34 @@ set -euo pipefail
 GPG_TTY="$(tty)"
 export GPG_TTY
 
-UNMOUNT="false"
+UNMOUNT_BOOT="false"
+UNMOUNT_EFI="false"
+
+if ! mountpoint /boot; then
+    mount /boot
+    UNMOUNT_BOOT="true"
+fi
 
 grep -E "^UUID=[0-9A-F]{4}-[0-9A-F]{4}[[:space:]]*/efi[a-z][[:space:]]*vfat[[:space:]]*" /etc/fstab | awk '{print $2}' | while read -r I; do
+
     if ! mountpoint "$I"; then
         mount "$I"
-        UNMOUNT="true"
+        UNMOUNT_EFI="true"
     fi
 
     ( grub-mkconfig | sed -n '/^### BEGIN \/etc\/grub.d\/10_linux ###$/,/^### END \/etc\/grub.d\/10_linux ###$/p'; grub-mkconfig | sed -n '/^### BEGIN \/etc\/grub.d\/40_custom ###$/,/^### END \/etc\/grub.d\/40_custom ###$/p' ) | sed -e "s/\$menuentry_id_option/--unrestricted --id/" | sed '/^[[:space:]]*else/,/^[[:space:]]*fi/d' | grep -v -e "^[[:space:]]*if" -e "^[[:space:]]*fi" -e "^[[:space:]]*load_video" -e "^[[:space:]]*insmod" > "${I}/grub.cfg"
 
     gpg --detach-sign "${I}/grub.cfg"
 
-    if [ "${UNMOUNT}" == "true" ]; then
+    if [ "${UNMOUNT_EFI}" == "true" ]; then
         umount "$I"
-        UNMOUNT="false"
+        UNMOUNT_EFI="false"
     fi
 done
+
+if [ "${UNMOUNT_BOOT}" == "true" ]; then
+    umount /boot
+    UNMOUNT_BOOT="false"
+fi
 
 gpgconf --kill all
