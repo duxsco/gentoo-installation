b1035c646dafb825d80ef2d8779e0fd19212622f
David Sardari <d@duxsco.de>
2022-01-21 21:57:01 +0100

    Update grub configuration without GnuPG signing

--- update_grub_cfg.sh
+++ update_grub_cfg.sh
@@ -2,9 +2,6 @@
 
 set -euo pipefail
 
-GPG_TTY="$(tty)"
-export GPG_TTY
-
 UNMOUNT_BOOT="false"
 UNMOUNT_EFI="false"
 
@@ -22,8 +19,6 @@ grep -E "^UUID=[0-9A-F]{4}-[0-9A-F]{4}[[:space:]]*/efi[a-z][[:space:]]*vfat[[:sp
 
     ( grub-mkconfig | sed -n '/^### BEGIN \/etc\/grub.d\/10_linux ###$/,/^### END \/etc\/grub.d\/10_linux ###$/p'; grub-mkconfig | sed -n '/^### BEGIN \/etc\/grub.d\/40_custom ###$/,/^### END \/etc\/grub.d\/40_custom ###$/p' ) | sed -e "s/\$menuentry_id_option/--unrestricted --id/" | sed '/^[[:space:]]*else/,/^[[:space:]]*fi/d' | grep -v -e "^[[:space:]]*if" -e "^[[:space:]]*fi" -e "^[[:space:]]*load_video" -e "^[[:space:]]*insmod" > "${I}/grub.cfg"
 
-    gpg --detach-sign "${I}/grub.cfg"
-
     if [ "${UNMOUNT_EFI}" == "true" ]; then
         umount "$I"
         UNMOUNT_EFI="false"
@@ -34,5 +29,3 @@ if [ "${UNMOUNT_BOOT}" == "true" ]; then
     umount /boot
     UNMOUNT_BOOT="false"
 fi
-
-gpgconf --kill all
